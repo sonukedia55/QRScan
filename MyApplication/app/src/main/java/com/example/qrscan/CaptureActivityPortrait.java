ec4707bd7e4272449d2ae4a1953bc19a4d31b598
sonukedia55 <sonukedia00@gmail.com>
2017-06-03 19:10:44 +0530

    qrCode scanner

---
+++ MyApplication/app/src/main/java/com/example/qrscan/CaptureActivityPortrait.java
@@ -0,0 +1,9 @@
+package com.example.qrscan;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by Sonu Kedia on 03/06/2017.
+ */
+public class CaptureActivityPortrait extends CaptureActivity {
+}
--- MyApplication/app/src/main/java/com/example/qrscan/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/qrscan/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.qrscan;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scan_qr;
+    EditText qrscan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scan_qr = (Button) findViewById(R.id.qr_scan);
+        final Activity activity = this;
+        qrscan = (EditText) findViewById(R.id.showqr);
+        scan_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intergrator = new IntentIntegrator(activity);
+                intergrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                intergrator.setPrompt("Scan");
+                intergrator.setCameraId(0);
+                intergrator.setBeepEnabled(false);
+                intergrator.setBarcodeImageEnabled(false);
+                intergrator.setCaptureActivity(CaptureActivityPortrait.class);
+                intergrator.initiateScan();
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+            } else {
+                qrscan.setText(result.getContents());
+            }
+        } else {
+
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
